61220a1e1c9d8876bca5ce2cd252cb8ff0237926
tolyo <anatolyostrovsky@gmail.com>
2013-11-28 01:19:01 +0200

    forcomp

---
+++ forcomp/.worksheet/src/forcomp.AnagramsWorksheet.scala
@@ -0,0 +1,8 @@
+package forcomp
+import forcomp.Anagrams._
+
+object AnagramsWorksheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(113); 
+  println("Welcome to the Scala worksheet")}
+
+  
+}
---
+++ forcomp/src/main/scala/forcomp/AnagramsWorksheet.sc
@@ -0,0 +1,8 @@
+package forcomp
+import forcomp.Anagrams._
+
+object AnagramsWorksheet {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+
+  
+}
