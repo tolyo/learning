8e048708ec4a1546b7cb03ee2525abd43e8a9e74
tolyo <anatolyostrovsky@gmail.com>
2013-11-28 01:15:16 +0200

    patmat

---
+++ patmat/.worksheet/src/patmat.HuffmanSheet.scala
@@ -0,0 +1,8 @@
+package patmat
+
+import Huffman._
+
+object HuffmanSheet {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(99); 
+  println("Welcome to the Scala worksheet")}
+ }
+  
---
+++ patmat/src/main/scala/patmat/HuffmanWorkSheet.sc
@@ -0,0 +1,8 @@
+package patmat
+
+import Huffman._
+
+object HuffmanSheet {
+  println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
+ }
+  
