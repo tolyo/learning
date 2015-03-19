121c72a75b177722635d44cb55bca957e39c6139
aostrovsky <anatolyostrovsky@gmail.com>
2015-03-19 11:29:13 +0200

    1.1

---
+++ src/ch1/UserArgument.java
@@ -0,0 +1,9 @@
+package ch1;
+
+public class UserArgument {
+    public static void main(String[] args) {
+        System.out.println("Hi, ");
+        System.out.println(args[0]);
+        System.out.println(", How are you");
+    }
+}
