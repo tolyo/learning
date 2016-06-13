ef5ed0678c4eeff25685c4cfbb52910ae6139181
aostrovsky <anatolyostrovsky@gmail.com>
2016-06-13 12:18:55 +0300

    Clean

--- src/ch1/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package ch1;
-
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
--- src/ch1/UserArgument.java
+++
@@ -1,9 +0,0 @@
-package ch1;
-
-public class UserArgument {
-    public static void main(String[] args) {
-        System.out.println("Hi, ");
-        System.out.println(args[0]);
-        System.out.println(", How are you");
-    }
-}
