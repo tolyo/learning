fb937542003eb1aa63dd11685c9e11c85fff1a8d
aostrovsky <anatolyostrovsky@gmail.com>
2017-05-12 16:14:14 +0300

    Loan resource

---
+++ boot/lender/src/main/java/io/fourfinanceit/rest/LoanResource.java
@@ -0,0 +1,31 @@
+package io.fourfinanceit.rest;
+
+import io.fourfinanceit.domain.Loan;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class LoanResource {
+
+    public Loan createLoan() {
+        // create a new loan for a customer
+
+        // create and save and application attempt
+        // validate application request
+
+        return null;
+    }
+
+
+    public Loan updateLoan() {
+        // extend load
+
+        // validate extention request
+
+        // return created
+        return null;
+    }
+
+
+}
