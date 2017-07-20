25e2a352395e745bed7432d995d9684a58000fcc
aostrovsky <anatolyostrovsky@gmail.com>
2017-07-20 14:02:24 +0300

    Build your own angular chapter 2 - init watcher

--- myangular/test/scope__spec.js
+++ myangular/test/scope__spec.js
@@ -9,7 +9,7 @@ describe("Scope", function () {
     scope.aProperty = 1;
 
     expect(scope.aProperty).toBe(1);
-  })
+  });
 });
 
 describe("digest", function () {
@@ -41,7 +41,7 @@ describe("digest", function () {
     scope.$digest();
 
     expect(watchFn).toHaveBeenCalledWith(scope);
-  })
+  });
 
   it("calls the listener function when the watched value changes", function () {
     scope.someValue = 'a';
@@ -68,5 +68,29 @@ describe("digest", function () {
 
     scope.$digest();
     expect(scope.counter).toBe(2);
+  });
+
+  it("calls listener when watch value is first undefined", function() {
+    scope.counter = 0;
+
+    scope.$watch(
+      function(scope) { return scope.someValue; },
+      function(newValue, oldValue, scope) { scope.counter++; }
+    );
+
+    scope.$digest();
+    expect(scope.counter).toBe(1);
+  });
+
+  it("calls listener with new value as old value the first time", function() {
+    scope.someValue = 123;
+    var oldValueGiven;
+    scope.$watch(
+      function(scope) { return scope.someValue; },
+      function(newValue, oldValue, scope) { oldValueGiven = oldValue; }
+    );
+    scope.$digest();
+
+    expect(oldValueGiven).toBe(123);
   });
 });
