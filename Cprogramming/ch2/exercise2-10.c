c7e2e5de0a123625aeb3f990ac99bf3a031dc618
tolyo <anatolyostrovsky@gmail.com>
2018-02-11 19:51:35 +0200

    C

---
+++ Cprogramming/ch2/exercise2-10.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int lower(int c)
+    {
+        return (c >= 'A' && c <= 'Z') ? c + 'a' - 'A' : c;
+    }
+    printf("Total %c\n", lower('A'));
+    printf("Total %c\n", lower('Z'));
+
+    printf("Total %c\n", lower('a'));
+    printf("Total %c\n", lower('z'));
+}
