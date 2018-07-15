b2ca481cbc8440eb17c229751e43bd98952e0527
tolyo <anatolyostrovsky@gmail.com>
2018-07-16 01:57:28 +0300

    programming elixir

--- programming_elixir/enum.ex
+++ programming_elixir/enum.ex
@@ -84,4 +84,25 @@ defmodule MyEnum do
   def primes(n) do
     for x <- span(2, n), is_prime(x), do: x
   end
+
+  # ListsAndRecursion-8
+  def get_orders() do
+    tax_rates = [ NC: 0.075, TX: 0.08 ]
+    orders = [
+      [id: 123, ship_to: :NC, net_amount: 100.00],
+      [id: 124, ship_to: :OK, net_amount: 35.50],
+      [id: 125, ship_to: :TX, net_amount: 24.00],
+      [id: 126, ship_to: :TX, net_amount: 44.00],
+      [id: 127, ship_to: :NC, net_amount: 25.00],
+      [id: 128, ship_to: :MA, net_amount: 10.00],
+      [id: 129, ship_to: :CA, net_amount: 102.00],
+      [id: 120, ship_to: :NC, net_amount: 50.00]
+    ]
+    
+    Enum.map(orders, fn x ->
+      rate = tax_rates[x[:ship_to]] || 0
+      Keyword.put(x, :total_amount, x[:net_amount] * (1 + rate))
+    end)
+  end
+
 end
