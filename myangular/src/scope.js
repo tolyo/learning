ee3ffe8e04509fcfbf1e2b9292580008b5a95e00
tolyo <anatolyostrovsky@gmail.com>
2019-10-20 22:58:41 +0300

    My angular cont chp 1

--- myangular/src/scope.js
+++ myangular/src/scope.js
@@ -1,35 +1,64 @@
 'use strict';
+
 var _ = require('lodash');
 
 function Scope() {
   this.$$watchers = [];
+  this.$$lastDirtyWatch = null;
 }
 
 function initWatchVal(){}
 
-Scope.prototype.$watch = function (watchFn, listenerFn) {
+Scope.prototype.$watch = function (watchFn, listenerFn, valueEq) {
   var watcher = {
     watchFn:    watchFn,
-    listenerFn: listenerFn,
+    listenerFn: listenerFn || function () { },
+    valueEq: !!valueEq,
     last: initWatchVal
   };
 
   this.$$watchers.push(watcher);
+  this.$$lastDirtyWatch = null;
 };
 
-Scope.prototype.$digest = function () {
+Scope.prototype.$digest = function() {
+  var ttl = 10;
+  var dirty;
+  this.$$lastDirtyWatch = null;
+  do {
+    dirty = this.$$digestOnce();
+    if (dirty && !(ttl--)) {
+      throw "10 digest iterations reached";
+    }
+  } while (dirty);
+};
+
+Scope.prototype.$$digestOnce = function () {
   var self = this;
-  var newValue, oldValue;
+  var newValue, oldValue, dirty;
   _.forEach(this.$$watchers, function (watcher) {
     newValue = watcher.watchFn(self);
     oldValue = watcher.last;
-    if (newValue !== oldValue) {
-      watcher.last = newValue;
+    if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+      self.$$lastDirtyWatch = watcher;
+      watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
       watcher.listenerFn(newValue,
         (oldValue === initWatchVal ? newValue : oldValue),
         self);
+        dirty = true;
+    } else if (self.$$lastDirtyWatch === watcher) {
+      return false;
     }
   });
+  return dirty;
 };
+
+Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
+  if (valueEq) {
+    return _.isEqual(newValue, oldValue);
+  } else {
+    return newValue === oldValue;
+  }
+}
 
 module.exports = Scope;
