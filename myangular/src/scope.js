b612dc24ff5bb89b2d127958b495245c9d75584c
tolyo <anatolyostrovsky@gmail.com>
2019-11-24 16:27:14 +0200

    My Angular chp 2

--- myangular/src/scope.js
+++ myangular/src/scope.js
@@ -9,6 +9,7 @@ function Scope() {
   this.$$applyAsyncQueue = [];
   this.$$applyAsyncId = null;
   this.$$postDigestQueue = [];
+  this.$$children = [];
   this.$$phase = null;
 }
 
@@ -120,28 +121,33 @@ Scope.prototype.$digest = function() {
 };
 
 Scope.prototype.$$digestOnce = function () {
+  var dirty;
+  var continueLoop = true;
   var self = this;
-  var newValue, oldValue, dirty;
-  _.forEachRight(this.$$watchers, function (watcher) {
-
-    try {
-      if (watcher) {
-        newValue = watcher.watchFn(self);
-        oldValue = watcher.last;
-        if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
-          self.$$lastDirtyWatch = watcher;
-          watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
-          watcher.listenerFn(newValue,
-            (oldValue === initWatchVal ? newValue : oldValue),
-            self);
-          dirty = true;
-        } else if (self.$$lastDirtyWatch === watcher) {
-          return false;
+  this.$$everyScope(function(scope) {
+    var newValue, oldValue;
+    _.forEachRight(scope.$$watchers, function(watcher) {
+      try {
+        if (watcher) {
+          newValue = watcher.watchFn(scope);
+          oldValue = watcher.last;
+          if (!scope.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+            self.$$lastDirtyWatch = watcher;
+            watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
+            watcher.listenerFn(newValue,
+                (oldValue === initWatchVal ? newValue : oldValue),
+                scope);
+            dirty = true;
+          } else if (self.$$lastDirtyWatch === watcher) {
+            continueLoop = false;
+            return false;
+          }
         }
+      } catch (e) {
+        console.error(e);
       }
-    } catch (e) {
-      console.error(e)
-    }
+    });
+    return continueLoop;
   });
   return dirty;
 };
@@ -157,7 +163,7 @@ Scope.prototype.$$areEqual = function (newValue, oldValue, valueEq) {
        isNaN(oldValue)
       );
   }
-}
+};
 
 Scope.prototype.$eval = function(expr, locals) {
   return expr(this, locals);
@@ -227,7 +233,21 @@ Scope.prototype.$$postDigest = function (expr) {
 };
 
 Scope.prototype.$new = function () {
-  return Object.create(this)
-}
+  var child = Object.create(this);
+  this.$$children.push(child);
+  child.$$watchers = [];
+  child.$$children = [];
+  return child;
+};
+
+Scope.prototype.$$everyScope = function(fn) {
+  if (fn(this)) {
+    return this.$$children.every(function(child) {
+      return child.$$everyScope(fn);
+    });
+  } else {
+    return false;
+  }
+};
 
 module.exports = Scope;
